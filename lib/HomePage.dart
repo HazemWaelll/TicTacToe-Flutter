3fc22c16dc59a5660d0b1c676dd33f20333a38b2
Hazem Wael <hazemwael0000@gmail.com>
2026-02-27 15:21:43 +0200

    Init commit

---
+++ lib/HomePage.dart
@@ -0,0 +1,239 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<String> displayX0 = ['', '', '', '', '', '', '', '', ''];
+
+  bool xturn = true;
+  static int xscore = 0;
+  static int oscore = 0;
+  int filledBoxes = 0;
+
+  static var pressStart2pFont = TextStyle(
+    fontFamily: "PressStart2P",
+    color: Colors.white,
+    letterSpacing: 3,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[850],
+      body: Column(
+        children: [
+          SizedBox(height: 15),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(30.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // X Score
+                      Text("Player X", style: pressStart2pFont),
+                      SizedBox(height: 20),
+                      Text(xscore.toString(), style: pressStart2pFont),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(30.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // O Score
+                      Text("Player O", style: pressStart2pFont),
+                      SizedBox(height: 20),
+                      Text(oscore.toString(), style: pressStart2pFont),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Text(
+            xturn ? "Player X Turn" : "Player O Turn",
+            style: pressStart2pFont,
+          ),
+          Expanded(
+            flex: 3,
+            child: GridView.builder(
+              itemCount: 9,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+              ),
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  onTap: () {
+                    tapped(index);
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey[700]!),
+                    ),
+                    child: Center(
+                      child: Text(
+                        displayX0[index],
+                        style: TextStyle(color: Colors.white, fontSize: 40),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text("TicTacToe Game", style: pressStart2pFont),
+                Text("@MADE BY HAZEMWAEL", style: pressStart2pFont),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void tapped(int index) {
+    setState(() {
+      if (xturn && displayX0[index] == '') {
+        displayX0[index] = 'X';
+        filledBoxes += 1;
+        xturn = !xturn;
+      } else if (!xturn && displayX0[index] == '') {
+        displayX0[index] = 'O';
+        filledBoxes += 1;
+        xturn = !xturn;
+      }
+      checkWinner();
+    });
+  }
+
+  void checkWinner() {
+    // checks 1st row
+    if (displayX0[0] == displayX0[1] &&
+        displayX0[0] == displayX0[2] &&
+        displayX0[0] != '') {
+      return showWinDialog(displayX0[0]);
+    }
+
+    // checks 2nd row
+    if (displayX0[3] == displayX0[4] &&
+        displayX0[3] == displayX0[5] &&
+        displayX0[3] != '') {
+      return showWinDialog(displayX0[3]);
+    }
+
+    // checks 3rd row
+    if (displayX0[6] == displayX0[7] &&
+        displayX0[6] == displayX0[8] &&
+        displayX0[6] != '') {
+      return showWinDialog(displayX0[6]);
+    }
+
+    // checks 1st column
+    if (displayX0[0] == displayX0[3] &&
+        displayX0[0] == displayX0[6] &&
+        displayX0[0] != '') {
+      return showWinDialog(displayX0[0]);
+    }
+
+    // checks 2nd column
+    if (displayX0[1] == displayX0[4] &&
+        displayX0[1] == displayX0[7] &&
+        displayX0[1] != '') {
+      return showWinDialog(displayX0[1]);
+    }
+
+    // checks 3rd column
+    if (displayX0[2] == displayX0[5] &&
+        displayX0[2] == displayX0[8] &&
+        displayX0[2] != '') {
+      return showWinDialog(displayX0[2]);
+    }
+
+    // checks diagonal
+    if (displayX0[0] == displayX0[4] &&
+        displayX0[0] == displayX0[8] &&
+        displayX0[0] != '') {
+      return showWinDialog(displayX0[0]);
+    }
+
+    // checks anti diagonal
+    if (displayX0[2] == displayX0[4] &&
+        displayX0[2] == displayX0[6] &&
+        displayX0[2] != '') {
+      return showWinDialog(displayX0[2]);
+    } else if (filledBoxes == 9) {
+      return showDrawDialog();
+    }
+  }
+
+  void showWinDialog(String winner) {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Winner is: $winner'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Play Again!'),
+              onPressed: () {
+                clearBoard();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+
+    if (winner == 'O') {
+      oscore += 1;
+    } else if (winner == 'X') {
+      xscore += 1;
+    }
+  }
+
+  void showDrawDialog() {
+    showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Draw'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Play Again!'),
+              onPressed: () {
+                clearBoard();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void clearBoard() {
+    setState(() {
+      for (int i = 0; i < 9; i++) {
+        displayX0[i] = '';
+      }
+    });
+
+    filledBoxes = 0;
+  }
+}
---
+++ lib/IntroPage.dart
@@ -0,0 +1,94 @@
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+
+class IntroPage extends StatefulWidget {
+  const IntroPage({super.key});
+
+  @override
+  State<IntroPage> createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  static var pressStart2pFont = TextStyle(
+    fontFamily: "PressStart2P",
+    color: Colors.white,
+    letterSpacing: 3,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[850],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(top: 120.0),
+                child: Text(
+                  "Tic Tac Toe",
+                  style: pressStart2pFont.copyWith(fontSize: 30),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: AvatarGlow(
+                duration: Duration(seconds: 2),
+                glowColor: Colors.white,
+                repeat: true,
+                startDelay: Duration(seconds: 1),
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey[900],
+                  radius: 80.0,
+                  child: ClipOval(
+                    // ClipOval is used to make the image fit the CircleAvatar
+                    child: Image.asset(
+                      "assets/images/tictactoelogo.png",
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(top: 80.0),
+                child: Text(
+                  "@MADEBYHAZEMWAEL",
+                  style: pressStart2pFont.copyWith(fontSize: 20),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                );
+              },
+              child: Padding(
+                padding: EdgeInsets.only(left: 40, right: 40, bottom: 60),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                  ),
+                  padding: EdgeInsets.all(30),
+                  child: Center(
+                    child: Text(
+                      'PLAY GAME',
+                      style: pressStart2pFont.copyWith(color: Colors.black),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
